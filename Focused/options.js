82ab0e5ed732b6aa1081dffa2799061b59c5a60d
ChislucaAna <139362209+ChislucaAna@users.noreply.github.com>
2024-08-10 11:26:40 +0300

    Add files via upload
    localstorage

--- Focused/options.js
+++ Focused/options.js
@@ -1,47 +1,42 @@
 function load_blocked_websites() {
     var textarea = document.getElementById("sites");
-    fetch('websites.txt')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(text => {
-            textarea.innerText = text;
-        })
-        .catch(error => {
-            console.error('Error loading file:', error.message);
-        });
+    const storedListString = localStorage.getItem('blockedsites');
+    const storedList = JSON.parse(storedListString);
+    textarea.innerText = storedList;
 }
 
 function load_blocked_features() {
-    fetch('features.txt')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(text => {
-            var content = text;
-            for (var i = 0; i < content.length; i++)
-                if (content[i] == "1") {
-                    const check = document.getElementById((i + 1).toString());
-                    check.checked = true;
-                }
-        })
-        .catch(error => {
-            console.error('Error loading file:', error.message);
-        });
+    const storedListString = localStorage.getItem('blockedfeatures');
+    const storedList = JSON.parse(storedListString);
+    for (var i = 0; i < storedList.length; i++)
+        if (storedList[i] == "1") {
+            const check = document.getElementById(i + 1);
+            check.checked = true;
+        }
 }
 
 async function save_changes() {
+    localStorage.clear();
+
+    const textarea = document.getElementById("sites").value;
+    const lines = textarea.split(/\r?\n/);
+    const blockedsites = JSON.stringify(lines);
+    localStorage.setItem('blockedsites', blockedsites);
+
+    var values="";
+    for (var i = 1; i <=3; i++)
+            if(document.getElementById((i).toString()).checked==true)
+                values+="1";
+            else
+                values+="0";
+    const blockedfeatures = JSON.stringify(values);
+    localStorage.setItem('blockedfeatures', blockedfeatures);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     load_blocked_websites();
     load_blocked_features();
+
     const saveButton = document.getElementById('save');
     saveButton.addEventListener('click', save_changes);
 });
